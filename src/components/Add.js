bb1ebf3d86bd0b72d339fb9919c70afc0071b606
mhmdAzzouzi <mhmdazzouzi1999@gmail.com>
2021-05-18 14:15:23 +0300

    fixed privacy issues

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,25 +1,24 @@
 import React, { useState } from "react";
-import ResultCard from './ResultCard'
+import ResultCard from "./ResultCard";
 
 function Add() {
   const [query, setQuery] = useState("");
-  const [results , setResults] = useState([])
+  const [results, setResults] = useState([]);
 
   const onChange = (e) => {
     e.preventDefault();
 
     setQuery(e.target.value);
     fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=d719cc77251f24ad6ea170bd0f8002b9&language=en-US&page=1&include_adult=false&query=${e.target.value}`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          if(!data.errors){
-              setResults(data.results)
-              console.log(data)
-          } else
-          setResults([])
-        })
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) {
+          setResults(data.results);
+          console.log(data);
+        } else setResults([]);
+      });
   };
 
   return (
@@ -33,17 +32,18 @@ function Add() {
               value={query}
               onChange={onChange}
             />
-           
           </div>
           {results.length > 0 && (
-                <ul>
-                    {results.map(movie => {
-                      return <li key={movie.id}>
-                           <ResultCard movie={movie} />
-                       </li>
-                    })}
-                </ul>
-            )}
+            <ul>
+              {results.map((movie) => {
+                return (
+                  <li key={movie.id}>
+                    <ResultCard movie={movie} />
+                  </li>
+                );
+              })}
+            </ul>
+          )}
         </div>
       </div>
     </div>
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,17 +1,18 @@
 export default (state, action) => {
   switch (action.type) {
-
     case "REMOVE_FROM_WATCHED":
-        return{
-            ...state,
-            watched: state.watched.filter(movie=> movie.id !== action.payload )
-        }
+      return {
+        ...state,
+        watched: state.watched.filter((movie) => movie.id !== action.payload),
+      };
     case "MOVE_TO_WATCHLIST":
-        return{
-            ...state , 
-            watched: state.watched.filter(movie=> movie.id !== action.payload.id),
-            watchlist:[action.payload , ...state.watchlist]
-        }
+      return {
+        ...state,
+        watched: state.watched.filter(
+          (movie) => movie.id !== action.payload.id
+        ),
+        watchlist: [action.payload, ...state.watchlist],
+      };
     case "ADD_MOVIE_TO_WATCHED":
       return {
         ...state,
